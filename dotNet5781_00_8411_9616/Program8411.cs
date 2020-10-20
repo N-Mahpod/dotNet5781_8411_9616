1f785a784a7676fed8f72741d6948b891cc1f6d5
Eli <elihowitt1@gmail.com>
2020-10-20 16:47:45 +0300

    Added the second 'Program' class with welcoming function.

---
+++ dotNet5781_00_8411_9616/Program8411.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8411_9616
+{
+    partial class Program
+    {
+        static partial void Welcome8411()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
