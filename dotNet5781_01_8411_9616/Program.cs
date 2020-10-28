c7f85cc8363f1fafb47f352b894c8b2ba47378b8
נריה מחפוד <mahpod@g.jct.ac.il>
2020-10-28 20:33:06 +0200

    Creating new project for ex1

---
+++ dotNet5781_01_8411_9616/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_8411_9616
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
