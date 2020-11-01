cdc86278e96d02f92396e9a41626b5b02634bd4d
נריה מחפוד <mahpod@g.jct.ac.il>
2020-11-01 21:57:33 +0200

    I created the class BusStation and Wrote the variables.

---
+++ dotNet5781_02_8411_9616/BusStation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_8411_9616
+{
+    class BusStation
+    {
+        private string busStationKey;
+        private double latitude;
+        private double longitude;
+        private string stationAdress;
+
+        private static bool IsProperKey(string k)
+        {
+            if (k.Length > 6 || !IsNumber(k))
+                return false;
+
+            while (k.Length < 6)
+            {
+                k.Insert(0, "0");
+            }
+
+            return true;
+        }
+
+        private static bool IsNumber(string n)
+        {
+            foreach (char ch in n)
+            {
+                if (ch < '0' || ch > '9')
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ dotNet5781_02_8411_9616/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_8411_9616
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What's up?");
+            Console.ReadKey();
+            Console.WriteLine("\bWow, immpressive!");
+            Console.ReadKey();
+
+        }
+    }
+}
