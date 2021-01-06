59d8071210dd4e10626dbdb2db936f8d6d85a855
נריה מחפוד <mahpod@g.jct.ac.il>
2021-01-06 13:15:43 +0200

    BLL

---
+++ BLL/BLLImp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.BLL_Api;
+using Dal_Api;
+
+
+namespace BLL
+{
+    class BLLImp :IBLL
+    {
+    }
+}
---
+++ BLL/BLL_Api/BLL_Factory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+
+namespace BLL.BLL_Api
+{
+    class BLL_Factory
+    {
+        public static IBLL GetBL(string type)
+        {
+            switch (type)
+            {
+                case "1":
+                    return new BLImp();
+                case "2":
+                //return new BLImp2();
+                default:
+                    return new BLImp();
+            }
+        }
+    }
+}
